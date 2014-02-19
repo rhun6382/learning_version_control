1af2c3b195884d3bde63ebf4f2c177c408019183
Bob Hunt <bobhunt123@gmail.com>
2014-02-19 11:38:29 +1100

    First version of the analysis

---
+++ analysis.R
@@ -0,0 +1,15 @@
+data <- read.csv("data/gapminder-FiveYearData.csv", stringsAsFactors=FALSE)
+
+
+#More on Bash .....notes from class
+#http://en.wikipedia.org/wiki/Bash_%28Unix_shell%29
+#Keyboard shortcuts
+#http://www.skorks.com/2009/09/bash-shortcuts-for-maximum-productivity/
+# git
+# Enter these three lines at the console, but replace the dummy name/email with yours.
+#  git config --global user.name "Bob Hunt"
+#git config --global user.email "bobhunt123@gmail.com"
+#git config --global color.ui "auto"
+# Check that this has worked by running this:
+#git config --list
+# (you should see your name and email here, and may see a few other things, but don't worry).
